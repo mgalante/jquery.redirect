0f97539fbf5af3c9ea6d37bb9a861f4ad5767353
mgalante <mgalante@gmail.com>
2015-07-08 18:21:07 -0300

    Update jquery.redirect.js

--- jquery.redirect.js
+++ jquery.redirect.js
@@ -1,11 +1,20 @@
 /* 
-Copyright (c) 2015 Miguel Galante
+JQuery Redirect v1.0
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Originaly created by Nemanja Avramovic, www.avramovic.info 
+Forked and mantained by Miguel Galante
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+Licensed under CC BY-SA 4.0 License: http://creativecommons.org/licenses/by-sa/4.0/
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+This means everyone is allowed to:
+
+Share — copy and redistribute the material in any medium or format
+Adapt — remix, transform, and build upon the material for any purpose, even commercially.
+Under following conditions:
+
+Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+
 */
 
 
