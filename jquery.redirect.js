5c88bb4ac4fd3a18846778dd1f1649126113cb29
mgalante <mgalante@gmail.com>
2015-07-10 12:53:02 -0300

    Clean code using jslint and jshint advices

--- jquery.redirect.js
+++ jquery.redirect.js
@@ -16,89 +16,94 @@ Attribution - You must give appropriate credit, provide a link to the license, a
 ShareAlike - If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
 
 */
+(function ($) {
+    'use strict';
 
+    /**
+     * jQuery Redirect
+     * @param {string} url - Url of the redirection
+     * @param {Object} values - (optional) An object with the data to send. If not present will look for values as QueryString in the target url.
+     * @param {string} method - (optional) The HTTP verb can be GET or POST (defaults to POST)
+     * @param {string} target - (optional) The target of the form. "_blank" will open the url in a new window.
+     */
+    $.redirect = function (url, values, method, target) {
+        method = (method && method.toUpperCase() === 'GET') ? 'GET' : 'POST';
 
-(function($){
-/**
- * jQuery Redirect
- * @param {string} url - Url of the redirection
- * @param {Object} values - (optional) An object with the data to send. If not present will look for values as QueryString in the target url.
- * @param {string} method - (optional) The HTTP verb can be GET or POST (defaults to POST)
- * @param {string} target - (optional) The target of the form. If you set "_blank" will open the url in a new window.
- */
-'use strict';
-	$.redirect = function( url, values, method, target) {
-		method = (method && method.toUpperCase() == 'GET') ? 'GET' : 'POST';
+        if (!values) {
+            var obj = $.parseUrl(url);
+            url = obj.url;
+            values = obj.params;
+        }
 
-		if (!values)
-		{
-			var obj = $.parse_url(url);
-			url = obj.url;
-			values = obj.params;
-		}
+        var form = $('<form>').attr({
+            method: method,
+            action: url,
+            target: target
+        });
 
-		var form = $('<form>').attr({
-			method: method,
-			action: url,
-			target: target
-		});
+        iterateValues(values, [], form);
+        form.appenTo('body').submit();
+    };
 
-		iterateValues(values, [], form);
-		$('body').append(form);
+    //Utility Functions
+    /**
+     * Url and QueryString Parser.
+     * @param {string} url - a Url to parse.
+     * @returns {object} an object with the parsed url with the following structure {url: URL, params:{ KEY: VALUE }}
+     */
+    $.parseUrl = function (url) {
+        if (url.indexOf('?') === -1) {
+            return {
+                url: url,
+                params: {}
+            };
+        }
+        var parts = url.split('?'),
+            query_string = parts[1],
+            elems = query_string.split('&');
+        url = parts[0];
 
-		form.submit();
-	};
+        var i, pair, obj = {};
+        for (i = 0; i < elems.length; i++) {
+            pair = elems[i].split('=');
+            obj[pair[0]] = pair[1];
+        }
 
-	//Utility Functions
-	$.parse_url = function(url)
-	{
-		if (url.indexOf('?') == -1){
-			return { url: url, params: {} };
-		}
-		var parts = url.split('?'),
-			query_string = parts[1],
-			elems = query_string.split('&');
-		url = parts[0];
+        return {
+            url: url,
+            params: obj
+        };
+    };
 
-		var obj = {};
-		for (var i in elems)
-		{
-			var pair = elems[i].split('=');
-			obj[pair[0]] = pair[1];
-		}
+    //Private Functions
+    var getInput = function (name, value, parent) {
+        var parentString;
+        if (parent.length > 0) {
+            parentString = parent[0];
+            var i;
+            for (i = 1; i < parent.length; i += 1) {
+                parentString += "[" + parent[i] + "]";
+            }
+            name = parentString + "[" + name + "]";
+        }
 
-		return {url: url, params: obj};
-	}; 
-	
-	//Private Functions
-	var getInput = function(name, value, parent) {
-		var parentString;
-		if( parent.length > 0 ) {
-			parentString = parent[0];
-			for(var i = 1; i < parent.length; ++i){
-				parentString += "[" + parent[i] + "]";
-			}
-			name = parentString + "[" + name + "]";
-		}
+        return $("<input>").attr({
+            type: "hidden",
+            name: name,
+            value: value
+        });
+    };
 
-		return $("<input>").attr({
-			type: "hidden",
-			name: name,
-			value: value
-		});
-	};
-
-	var iterateValues = function(values, parent, form) {
-		var iterateParent = [];
-		for(var i in values)
-		{
-			if( typeof values[i] == "object" ) {
-				iterateParent = parent.slice();
-				iterateParent.push(i);
-				iterateValues(values[i], iterateParent, form);
-			} else {
-				getInput(i, values[i], parent).appendTo(form);
-			}
-		}
-	};
-})(jQuery);
+    var iterateValues = function (values, parent, form) {
+        var i, iterateParent = [];
+        for (i in values) {
+            if (typeof values[i] === "object") {
+                iterateParent = parent.slice();
+                iterateParent.push(i);
+                iterateValues(values[i], iterateParent, form);
+            } else {
+                getInput(i, values[i], parent).appendTo(form);
+            }
+        }
+    };
+}(jQuery));
