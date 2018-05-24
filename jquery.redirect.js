d987ef1f441f9f84aaf2a109e64688d5da05fe74
fab-c <6087799+fab-c@users.noreply.github.com>
2018-05-24 09:12:49 +0200

    Fix funciton overloading

--- jquery.redirect.js
+++ jquery.redirect.js
@@ -17,9 +17,9 @@ ShareAlike - If you remix, transform, or build upon the material, you must distr
 
 */
 (function ($) {
-	'use strict';
+    'use strict';
 
-	/**
+    /**
      * jQuery Redirect
      * @param {string} url - Url of the redirection
      * @param {Object} values - (optional) An object with the data to send. If not present will look for values as QueryString in the target url.
@@ -30,146 +30,153 @@ ShareAlike - If you remix, transform, or build upon the material, you must distr
      */
 
     //Defaults configuration
-	var defaults = {
-	    url: null,
-	    values: null,
-	    method: "POST",
-	    target: null,
-	    traditional: false,
-	    redirectTop: false
-	};
+    var defaults = {
+        url: null,
+        values: null,
+        method: "POST",
+        target: null,
+        traditional: true,
+        redirectTop: false
+    };
 
-	$.redirect = function (url, values, method, target, traditional, redirectTop) {
-	    var opts = {
-	        url: url,
-	        values: values,
-	        method: method,
-	        target: target,
-	        traditional: traditional,
-	        redirectTop: redirectTop
-	    };
-	    var config = $.extend(defaults, opts);
-	    $.redirect(config);
-	};
+    $.redirect = function redirect() {
+        if (arguments.length === 1 && typeof (arguments[0]) === 'object')
+            return redirectOneArg.apply(this, arguments);
 
-	$.redirect = function (opts) {
-	    var config = $.extend(defaults, opts);
-	    var generatedForm = $.redirect.getForm(config.url, config.values, config.method, config.target, config.traditional);
-	    $('body', config.redirectTop ? window.top.document : undefined).append(generatedForm.form);
-	    generatedForm.submit();
-	};
+        return redirectArgs.apply(this, arguments);
+    }
 
-	$.redirect.getForm = function (url, values, method, target, traditional) {
-		method = (method && ["GET", "POST", "PUT", "DELETE"].indexOf(method.toUpperCase()) !== -1) ? method.toUpperCase() : 'POST';
+    function redirectArgs (url, values, method, target, traditional, redirectTop) {
+        var opts = {
+            url: url,
+            values: values,
+            method: method,
+            target: target,
+            traditional: traditional,
+            redirectTop: redirectTop
+        };
+        var config = $.extend(defaults, opts);
+        redirectOneArg(config);
+    };
 
-		url = url.split("#");
-		var hash = url[1] ? ("#" + url[1]) : "";
-		url = url[0];
+    function redirectOneArg (opts) {
+        var config = $.extend(defaults, opts);
+        var generatedForm = $.redirect.getForm(config.url, config.values, config.method, config.target, config.traditional);
+        $('body', config.redirectTop ? window.top.document : undefined).append(generatedForm.form);
+        generatedForm.submit();
+    };
 
-		if (!values) {
-			var obj = $.parseUrl(url);
-			url = obj.url;
-			values = obj.params;
-		}
+    $.redirect.getForm = function (url, values, method, target, traditional) {
+        method = (method && ["GET", "POST", "PUT", "DELETE"].indexOf(method.toUpperCase()) !== -1) ? method.toUpperCase() : 'POST';
 
-		values = removeNulls(values);
+        url = url.split("#");
+        var hash = url[1] ? ("#" + url[1]) : "";
+        url = url[0];
 
-		var form = $('<form>')
+        if (!values) {
+            var obj = $.parseUrl(url);
+            url = obj.url;
+            values = obj.params;
+        }
+
+        values = removeNulls(values);
+
+        var form = $('<form>')
             .attr("method", method)
             .attr("action", url + hash);
 
 
-		if (target) {
-			form.attr("target", target);
-		}
+        if (target) {
+            form.attr("target", target);
+        }
 
-		var submit = form[0].submit;
-		iterateValues(values, [], form, null, traditional);
+        var submit = form[0].submit;
+        iterateValues(values, [], form, null, traditional);
 
-		return { form: form, submit: function () { submit.call(form[0]); } };
-	}
-	//Utility Functions
-	/**
+        return { form: form, submit: function () { submit.call(form[0]); } };
+    }
+    //Utility Functions
+    /**
      * Url and QueryString Parser.
      * @param {string} url - a Url to parse.
      * @returns {object} an object with the parsed url with the following structure {url: URL, params:{ KEY: VALUE }}
      */
-	$.parseUrl = function (url) {
+    $.parseUrl = function (url) {
 
-		if (url.indexOf('?') === -1) {
-			return {
-				url: url,
-				params: {}
-			};
-		}
-		var parts = url.split('?'),
+        if (url.indexOf('?') === -1) {
+            return {
+                url: url,
+                params: {}
+            };
+        }
+        var parts = url.split('?'),
             query_string = parts[1],
             elems = query_string.split('&');
-		url = parts[0];
+        url = parts[0];
 
-		var i, pair, obj = {};
-		for (i = 0; i < elems.length; i += 1) {
-			pair = elems[i].split('=');
-			obj[pair[0]] = pair[1];
-		}
+        var i, pair, obj = {};
+        for (i = 0; i < elems.length; i += 1) {
+            pair = elems[i].split('=');
+            obj[pair[0]] = pair[1];
+        }
 
-		return {
-			url: url,
-			params: obj
-		};
-	};
+        return {
+            url: url,
+            params: obj
+        };
+    };
 
-	//Private Functions
-	var getInput = function (name, value, parent, array, traditional) {
-		var parentString;
-		if (parent.length > 0) {
-			parentString = parent[0];
-			var i;
-			for (i = 1; i < parent.length; i += 1) {
-				parentString += "[" + parent[i] + "]";
-			}
+    //Private Functions
+    var getInput = function (name, value, parent, array, traditional) {
+        var parentString;
+        if (parent.length > 0) {
+            parentString = parent[0];
+            var i;
+            for (i = 1; i < parent.length; i += 1) {
+                parentString += "[" + parent[i] + "]";
+            }
 
-			if (array) {
-				if (traditional)
-					name = parentString;
-				else
-					name = parentString + "[" + name + "]";
-			} else {
-				name = parentString + "[" + name + "]";
-			}
-		}
+            if (array) {
+                if (traditional)
+                    name = parentString;
+                else
+                    name = parentString + "[" + name + "]";
+            } else {
+                name = parentString + "[" + name + "]";
+            }
+        }
 
-		return $("<input>").attr("type", "hidden")
+        return $("<input>").attr("type", "hidden")
             .attr("name", name)
             .attr("value", value);
-	};
+    };
 
-	var iterateValues = function (values, parent, form, isArray, traditional) {
-		var i, iterateParent = [];
-		Object.keys(values).forEach(function (i) {
-			if (typeof values[i] === "object") {
-				iterateParent = parent.slice();
-				iterateParent.push(i);
-				iterateValues(values[i], iterateParent, form, Array.isArray(values[i]), traditional);
-			} else {
-				form.append(getInput(i, values[i], parent, isArray, traditional));
-			}
-		});
-	};
+    var iterateValues = function (values, parent, form, isArray, traditional) {
+        var i, iterateParent = [];
+        Object.keys(values).forEach(function (i) {
+            if (typeof values[i] === "object") {
+                iterateParent = parent.slice();
+                iterateParent.push(i);
+                iterateValues(values[i], iterateParent, form, Array.isArray(values[i]), traditional);
+            } else {
+                form.append(getInput(i, values[i], parent, isArray, traditional));
+            }
+        });
+    };
 
-	var removeNulls = function (values) {
-		var propNames = Object.getOwnPropertyNames(values);
-		for (var i = 0; i < propNames.length; i++) {
-			var propName = propNames[i];
-			if (values[propName] === null || values[propName] === undefined) {
-				delete values[propName];
-			} else if (typeof values[propName] === 'object') {
-				values[propName] = removeNulls(values[propName]);
-			} else if (values[propName].length < 1) {
-				delete values[propName];
-			}
-		}
-		return values;
-	};
+    var removeNulls = function (values) {
+        var propNames = Object.getOwnPropertyNames(values);
+        for (var i = 0; i < propNames.length; i++) {
+            var propName = propNames[i];
+            if (values[propName] === null || values[propName] === undefined) {
+                delete values[propName];
+            } else if (typeof values[propName] === 'object') {
+                values[propName] = removeNulls(values[propName]);
+            } else if (values[propName].length < 1) {
+                delete values[propName];
+            }
+        }
+        return values;
+    };
 
 }(window.jQuery || window.Zepto || window.jqlite));
