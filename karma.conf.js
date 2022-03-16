7d195b6078d4d0373cf5ea4159c7bda87e55c647
Iñaki Guastalli <iaguastalli@gmail.com>
2022-03-16 23:58:08 +0000

    Linted

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 // Karma configuration
 module.exports = function (config) {
